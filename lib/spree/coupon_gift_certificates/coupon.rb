d22d8b6765c41ba465174fce35d14397822565af
David Busse <dpb@essub.com>
2012-02-22 13:09:03 -0600

    use regex for all gc checks

--- lib/spree/coupon_gift_certificates/coupon.rb
+++ lib/spree/coupon_gift_certificates/coupon.rb
@@ -7,10 +7,13 @@ module Spree::CouponGiftCertificates::Coupon
   end
  
   def cgc_eligible?(order)
+    increments = OptionType.find_by_name("gift_cert_increments").option_values.map {|v| v.name.split("$").last }
+    gc_regex = Regexp.new("^RT[#{increments.join('|')}]\d{4,}")
+    
     return false if expires_at and Time.now > expires_at
     return false if usage_limit and coupon_credits.with_order.count >= usage_limit
     return false if starts_at and Time.now < starts_at
-    return false if code =~ /^giftcert-/ && calculator.preferred_amount == 0
+    return false if code =~ gc_regex && calculator.preferred_amount == 0
     true
   end
 end
--- lib/spree/coupon_gift_certificates/coupon_credit.rb
+++ lib/spree/coupon_gift_certificates/coupon_credit.rb
@@ -7,9 +7,12 @@ module Spree::CouponGiftCertificates::CouponCredit
   end
 
   def cgc_calculate_coupon_credit
+    increments = OptionType.find_by_name("gift_cert_increments").option_values.map {|v| v.name.split("$").last }
+    gc_regex = Regexp.new("^RT[#{increments.join('|')}]\d{4,}")
+    
     return 0 if order.line_items.empty?
     amount = adjustment_source.calculator.compute(order.line_items).abs
-    order_total = adjustment_source.code.include?('giftcert-') ? order.item_total + order.charges.total : order.item_total
+    order_total = adjustment_source.code =~ gc_regex ? order.item_total + order.charges.total : order.item_total
     amount = order_total if amount > order_total
     -1 * amount
   end 
